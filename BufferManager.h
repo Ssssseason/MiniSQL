b889ea1273169e79713d94453b9355d43058ad23
ZhouxZhan <zhouxzhan@zju.edu.cn>
2017-06-01 19:15:11 +0800

    add BM.h

--- BufferManager.h
+++ BufferManager.h
@@ -16,9 +16,11 @@ public:
     BufferManager();
     ~BufferManager();
     Block* find_block(const string file_name, const int offset);
-    void write_file(Block* block_to_write);
-    void read_file(const string file_name);
+    void write_to_file(Block* block_to_write);
+//    void read_file(const string file_name);
+    void read_to_block(const string file_name, const int offset, Block* block_to_read);
     void set_block_front(Block* moved_block);
+    const int file_block(string file_name);
     const unsigned int get_block_num()
     {
         return Buffer_Pool.size();
