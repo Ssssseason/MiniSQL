3024ffb02b5bb6d61c35edb5f800ab4a3be8e752
ZhouxZhan <zhouxzhan@zju.edu.cn>
2017-06-01 19:17:57 +0800

    BM.cpp no-test

--- BufferManager.cpp
+++ BufferManager.cpp
@@ -17,20 +17,27 @@ Block* BufferManager::find_block(const string file_name, const int offset)
     for(auto ele : Buffer_Pool)
     {
         if(file_name == ele->file_name && offset == ele->offset)
+        {
+            set_block_front(ele);
             return ele;
+        }
     }
     int block_num = get_block_num();
     if(block_num < MAX_BLOCK)
     {
         Buffer_Pool.push_front(new Block);
+        read_to_block(file_name, offset, Buffer_Pool.front());
     }
     else
     {
-		set_block_front(Buffer_Pool.back());
         if(Buffer_Pool.front()->is_pinned == true)
             return nullptr;
         else if(Buffer_Pool.front()->is_dirty == true)
-            write_file(Buffer_Pool.front());
+        {
+		    set_block_front(Buffer_Pool.back());
+            write_to_file(Buffer_Pool.front());
+        }
+        read_to_block(file_name, offset, Buffer_Pool.front());
     }
     return Buffer_Pool.front();
 }
@@ -48,43 +55,57 @@ void BufferManager::set_block_front(Block* moved_block)
     Buffer_Pool.push_front(moved_block);
 }
 
-void BufferManager::write_file(Block* block_to_write)
+void BufferManager::write_to_file(Block* block_to_write)
 {
     block_to_write->is_pinned = false;
     fstream out(block_to_write->file_name, ios::out | ios::binary);
     out.seekp(block_to_write->offset, ios::beg);
-    out.write(block_to_write->record, block_to_write->get_record_length());
+    out.write(block_to_write->record, Block::BLOCK_SIZE);
     block_to_write->is_dirty = false;
     out.close();
     block_to_write->is_pinned = true;
 }
 
-void BufferManager::read_file(const string file_name)
+
+void BufferManager::read_to_block(const string file_name, const int offset, Block* block_to_read)
+{
+    block_to_read->is_pinned = true;
+    fstream in(file_name, ios::in | ios::binary);
+    in.seekg(block_to_read->offset, ios::beg);
+    in.read(block_to_read->record, Block::BLOCK_SIZE);
+    in.close();
+    block_to_read->file_name = file_name;
+    block_to_read->offset = offset;
+    block_to_read->is_pinned = false;
+}
+// void BufferManager::read_file(const string file_name)
+// {
+//     fstream in(file_name, ios::in | ios::binary);
+//     int offset_to_end = ios::end;
+//     int read_offset = 0;
+//     while(!in.eof())
+//     {
+//         if(offset_to_end >= Block::BLOCK_SIZE)
+//             read_offset = offset_to_end - Block::BLOCK_SIZE;
+//         else
+//             read_offset = offset_to_end;
+//         Block* block = find_block(file_name, read_offset);
+//         block->is_pinned = true;
+//         block->offset = in.tellg();
+//         in.read(block->record, Block::BLOCK_SIZE);
+//         block->file_name = file_name;
+//         set_block_front(block);
+//         block->is_pinned = false;
+//     }
+//     in.close();
+// }
+
+const int BufferManager::file_block(string file_name)
 {
     fstream in(file_name, ios::in | ios::binary);
-    int offset_to_end = ios::end;
-    int read_offset = 0;
-    while(!in.eof())
-    {
-        if(offset_to_end >= Block::BLOCK_SIZE)
-            read_offset = offset_to_end - Block::BLOCK_SIZE;
-        else
-            read_offset = offset_to_end;
-        Block* block = find_block(file_name, read_offset);
-        block->is_pinned = true;
-        block->offset = in.tellg();
-        if(offset_to_end >= Block::BLOCK_SIZE)
-        {
-            in.read(block->record, Block::BLOCK_SIZE);
-            offset_to_end -= Block::BLOCK_SIZE;
-        }   
-        else
-            in.read(block->record, ios::end);
-        block->file_name = file_name;
-        set_block_front(block);
-        block->is_pinned = false;
-    }
+    const int temp = (ios::end - ios::end) / Block::BLOCK_SIZE;
     in.close();
+    return temp;
 }
 
 Block::Block():
@@ -94,13 +115,13 @@ Block::Block():
     is_dirty(false),
     is_pinned(false)
 {
-    memset(record, -1, BLOCK_SIZE);
+    memset(record, 0, BLOCK_SIZE);
 }
 
 const int Block::get_record_length()
 {
     for(int i = 0; i < BLOCK_SIZE; ++i)
-        if(record[i] == -1)
+        if(record[i] == 0)
             return i + 1;
     return BLOCK_SIZE;
 }
